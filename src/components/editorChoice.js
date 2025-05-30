f8dfbe57e1803d15906820ae59a1bcbc08e081a4
Muhammad Shoaib <mshoaibkaglur@gmail.com>
2025-05-30 13:01:18 +0500

    a

--- src/components/editorChoice.js
+++ src/components/editorChoice.js
@@ -1,106 +1,48 @@
-import React from "react";
-import editorChoice1 from "../assets/images/editor-choice1.webp";
-import editorChoice2 from "../assets/images/editor-choice2.webp";
-import editorChoice3 from "../assets/images/editor-choice3.webp";
-import editorChoice4 from "../assets/images/reddit.webp";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+const API_URL =
+  "https://valourwealthdjango-production.up.railway.app/api/editors-choice/";
 
-function editorChoice() {
+function EditorChoice() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get(API_URL).then((res) => {
+      setPosts(res.data);
+    });
+  }, []);
+
   return (
     <section className="section_edit_choice">
       <div className="container">
-        <div class="sec_heading">
-          <h2 class="pb-4">Editors' Choice</h2>
+        <div className="sec_heading">
+          <h2 className="pb-4">Editors' Choice</h2>
         </div>
         <div className="row">
-          <div className="col-lg-3">
-            <div className="editors-choice">
-              <a href="">
-                <div className="choice_img">
-                <img
-                  className="obj_fit"
-                  src={editorChoice1}
-                  alt="editorChoiceimg"
-                />
-                </div>
-                <div className="editor_desc">
-                  <h3>Markets</h3>
-                  <p>
-                    This is How a Cheap Balanced Portfolio Flattens Those Pricey
-                    Hedge Funds
-                  </p>
-                </div>
-              </a>
+          {posts.map((post) => (
+            <div key={post.id} className="col-lg-3">
+              <div className="editors-choice">
+                <a href={`/blog/${post.slug}`}>
+                  <div className="choice_img">
+                    <img
+                      className="obj_fit"
+                      src={post.image_url}
+                      alt="editorChoiceimg"
+                    />
+                  </div>
+                  <div className="editor_desc">
+                    <h3>{post.category}</h3>
+                    <p>{post.description}</p>
+                  </div>
+                </a>
+              </div>
             </div>
-          </div>
-
-          <div className="col-lg-3">
-            <div className="editors-choice">
-              <a href="">
-                <div className="choice_img">
-                <img
-                  className="obj_fit"
-                  src={editorChoice2}
-                  alt="editorChoiceimg"
-                />
-                </div>
-                <div className="editor_desc">
-                  <h3>Markets</h3>
-                  <p>
-                  The 10-year Yield Drops to Its Lowest Level in Three Weeks as the Fed's Preferred Inflation Measure Rises
-                  </p>
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div className="col-lg-3">
-            <div className="editors-choice">
-              <a href="">
-                <div className="choice_img">
-                <img
-                  className="obj_fit"
-                  src={editorChoice3}
-                  alt="editorChoiceimg"
-                />
-                </div>
-                <div className="editor_desc">
-                  <h3>Markets</h3>
-                  <p>
-                  The Stock of AMD Faces a New Risk And This One Has Nothing to Do With AI
-                  </p>
-                </div>
-              </a>
-            </div>
-          </div>
-
-          <div className="col-lg-3">
-            <div className="editors-choice">
-              <a href="">
-                <div className="choice_img">
-                <img
-                  className="obj_fit"
-                  src={editorChoice4}
-                  alt="editorChoiceimg"
-                />
-
-                </div>
-             
-                <div className="editor_desc">
-                  <h3>Markets</h3>
-                  <p>
-                  Google's 'Curveball' of Reddit Traffic Offers Investors a Buying Opportunity
-                  </p>
-                </div>
-              </a>
-            </div>
-          </div>
-
-
+          ))}
         </div>
       </div>
     </section>
   );
 }
 
-export default editorChoice;
+export default EditorChoice;
