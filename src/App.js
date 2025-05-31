e181c75acf058dfbba5cbe036279d0d5f9816feb
Muhammad Shoaib <mshoaibkaglur@gmail.com>
2025-05-31 14:52:50 +0500

    agdgeg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,213 @@
+// import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+// import Dashboard from "./components/Dashboard";
+// import Layout from "./components/layout";
+// import News from "./components/News";
+// import AboutUs from "./pages/AboutUs";
+// import ContactPage from "./pages/ContactPage";
+// import CryptoDetails from "./pages/CryptoDetails";
+// import CustomerSupport from "./pages/CustomerSupport";
+// import EventsDetails from "./pages/EventsDetails";
+// import Feature from "./pages/Feature";
+// import Home from "./pages/Home";
+// import LegalDisclaimer from "./pages/LegalDisclaimer";
+// import Login from "./pages/Login";
+// import MarketDetails from "./pages/MarketDetails";
+// import PrivacyPolicy from "./pages/Privacypolicy";
+// import Review from "./pages/Review";
+// import TechnologyDetail from "./pages/TechnologyDetail";
+// import TermsService from "./pages/TermsService";
+// import UsePolicy from "./pages/UsePolicy";
+// import WealthDetails from "./pages/WealthDetails";
+// // import OurAnalyst from "./pages/OurAnalyst";
+// import Benefits from "./components/DashboardPlatinum/Benefits";
+// import ViewChallenge from "./components/DashboardPlatinum/ViewChallenge";
+// import ReleaseNotes from "./components/DashboardSidebarComp/ReleaseNotes";
+// import CourseDetails from "./pages/CourseDetails";
+// import PlatinumDashboard from "./pages/PlatinumDashboard";
+// import Products from "./pages/Products";
+// import RequestDemo from "./pages/RequestDemo";
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/benefits" element={<Benefits />} />
+//         <Route path="/releasenotes" element={<ReleaseNotes />} />
+//         <Route
+//           path="/"
+//           element={
+//             <Layout>
+//               <Home />
+//             </Layout>
+//           }
+//         />
+//         <Route
+//           path="/about"
+//           element={
+//             <Layout>
+//               <AboutUs />
+//             </Layout>
+//           }
+//         />
+//         <Route
+//           path="/features"
+//           element={
+//             <Layout>
+//               {" "}
+//               <Feature />{" "}
+//             </Layout>
+//           }
+//         />
+//         <Route
+//           path="/market-details"
+//           element={
+//             <Layout>
+//               {" "}
+//               <MarketDetails />{" "}
+//             </Layout>
+//           }
+//         />
+//         <Route
+//           path="/technology-details"
+//           element={
+//             <Layout>
+//               {" "}
+//               <TechnologyDetail />{" "}
+//             </Layout>
+//           }
+//         />
+//         <Route
+//           path="/wealth-details"
+//           element={
+//             <Layout>
+//               {" "}
+//               <WealthDetails />{" "}
+//             </Layout>
+//           }
+//         />
+//         <Route
+//           path="/events-details"
+//           element={
+//             <Layout>
+//               {" "}
+//               <EventsDetails />{" "}
+//             </Layout>
+//           }
+//         />
+//         <Route
+//           path="/crypto-details"
+//           element={
+//             <Layout>
+//               {" "}
+//               <CryptoDetails />{" "}
+//             </Layout>
+//           }
+//         />
+//         <Route
+//           path="/customer-support"
+//           element={
+//             <Layout>
+//               {" "}
+//               <CustomerSupport />{" "}
+//             </Layout>
+//           }
+//         />
+//         <Route
+//           path="/leave-a-review"
+//           element={
+//             <Layout>
+//               {" "}
+//               <Review />{" "}
+//             </Layout>
+//           }
+//         />
+//         <Route
+//           path="/latest-news"
+//           element={
+//             <Layout>
+//               {" "}
+//               <News />{" "}
+//             </Layout>
+//           }
+//         />
+//         <Route
+//           path="/terms-of-service"
+//           element={
+//             <Layout>
+//               {" "}
+//               <TermsService />{" "}
+//             </Layout>
+//           }
+//         />
+//         <Route
+//           path="/acceptable-use-policy"
+//           element={
+//             <Layout>
+//               {" "}
+//               <UsePolicy />{" "}
+//             </Layout>
+//           }
+//         />
+//         <Route
+//           path="/legal-disclaimer"
+//           element={
+//             <Layout>
+//               {" "}
+//               <LegalDisclaimer />{" "}
+//             </Layout>
+//           }
+//         />
+//         {/* <Route path="/our-analyst" element={<Layout> <OurAnalyst/> </Layout>} /> */}
+//         <Route
+//           path="/our-products"
+//           element={
+//             <Layout>
+//               {" "}
+//               <Products />{" "}
+//             </Layout>
+//           }
+//         />
+
+//         {/* <Route path="/academy/:courseId" element={<ValourAcademy />} /> */}
+
+//         <Route path="/academy/:courseId" element={<CourseDetails />} />
+//         <Route
+//           path="/request-a-demo"
+//           element={
+//             <Layout>
+//               {" "}
+//               <RequestDemo />{" "}
+//             </Layout>
+//           }
+//         />
+//         <Route path="/dashboard" element={<Dashboard />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/Privacy-Policy" element={<PrivacyPolicy />} />
+//         <Route path="/contact" element={<ContactPage />}></Route>
+//         {/* =========Platinum Dashboard======= */}
+//         <Route
+//           path="/platinum-dashboard"
+//           element={<PlatinumDashboard />}
+//         ></Route>
+//         {/* <Route path="/view-challenges" element={<ViewChallenge />}></Route> */}
+//         <Route path="/challenges/:id" element={<ViewChallenge />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+import { useEffect, useState } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
+import Benefits from "./components/DashboardPlatinum/Benefits";
+import ViewChallenge from "./components/DashboardPlatinum/ViewChallenge";
+import ReleaseNotes from "./components/DashboardSidebarComp/ReleaseNotes";
 import Layout from "./components/layout";
 import News from "./components/News";
 import AboutUs from "./pages/AboutUs";
 import ContactPage from "./pages/ContactPage";
+import CourseDetails from "./pages/CourseDetails";
 import CryptoDetails from "./pages/CryptoDetails";
 import CustomerSupport from "./pages/CustomerSupport";
 import EventsDetails from "./pages/EventsDetails";
@@ -12,21 +216,35 @@ import Home from "./pages/Home";
 import LegalDisclaimer from "./pages/LegalDisclaimer";
 import Login from "./pages/Login";
 import MarketDetails from "./pages/MarketDetails";
+import PlatinumDashboard from "./pages/PlatinumDashboard";
 import PrivacyPolicy from "./pages/Privacypolicy";
+import Products from "./pages/Products";
+import RequestDemo from "./pages/RequestDemo";
 import Review from "./pages/Review";
 import TechnologyDetail from "./pages/TechnologyDetail";
 import TermsService from "./pages/TermsService";
 import UsePolicy from "./pages/UsePolicy";
 import WealthDetails from "./pages/WealthDetails";
-// import OurAnalyst from "./pages/OurAnalyst";
-import Benefits from "./components/DashboardPlatinum/Benefits";
-import ViewChallenge from "./components/DashboardPlatinum/ViewChallenge";
-import ReleaseNotes from "./components/DashboardSidebarComp/ReleaseNotes";
-import CourseDetails from "./pages/CourseDetails";
-import PlatinumDashboard from "./pages/PlatinumDashboard";
-import Products from "./pages/Products";
-import RequestDemo from "./pages/RequestDemo";
+
+import "./App.css";
+import logo from "./assets/images/Valour_Wealth.png";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 3000); // 3 seconds
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="preloader">
+        <img src={logo} alt="Loading..." className="rotating-logo" />
+      </div>
+    );
+  }
+
   return (
     <Router>
       <Routes>
@@ -52,8 +270,7 @@ function App() {
           path="/features"
           element={
             <Layout>
-              {" "}
-              <Feature />{" "}
+              <Feature />
             </Layout>
           }
         />
@@ -61,8 +278,7 @@ function App() {
           path="/market-details"
           element={
             <Layout>
-              {" "}
-              <MarketDetails />{" "}
+              <MarketDetails />
             </Layout>
           }
         />
@@ -70,8 +286,7 @@ function App() {
           path="/technology-details"
           element={
             <Layout>
-              {" "}
-              <TechnologyDetail />{" "}
+              <TechnologyDetail />
             </Layout>
           }
         />
@@ -79,8 +294,7 @@ function App() {
           path="/wealth-details"
           element={
             <Layout>
-              {" "}
-              <WealthDetails />{" "}
+              <WealthDetails />
             </Layout>
           }
         />
@@ -88,8 +302,7 @@ function App() {
           path="/events-details"
           element={
             <Layout>
-              {" "}
-              <EventsDetails />{" "}
+              <EventsDetails />
             </Layout>
           }
         />
@@ -97,8 +310,7 @@ function App() {
           path="/crypto-details"
           element={
             <Layout>
-              {" "}
-              <CryptoDetails />{" "}
+              <CryptoDetails />
             </Layout>
           }
         />
@@ -106,8 +318,7 @@ function App() {
           path="/customer-support"
           element={
             <Layout>
-              {" "}
-              <CustomerSupport />{" "}
+              <CustomerSupport />
             </Layout>
           }
         />
@@ -115,8 +326,7 @@ function App() {
           path="/leave-a-review"
           element={
             <Layout>
-              {" "}
-              <Review />{" "}
+              <Review />
             </Layout>
           }
         />
@@ -124,8 +334,7 @@ function App() {
           path="/latest-news"
           element={
             <Layout>
-              {" "}
-              <News />{" "}
+              <News />
             </Layout>
           }
         />
@@ -133,8 +342,7 @@ function App() {
           path="/terms-of-service"
           element={
             <Layout>
-              {" "}
-              <TermsService />{" "}
+              <TermsService />
             </Layout>
           }
         />
@@ -142,8 +350,7 @@ function App() {
           path="/acceptable-use-policy"
           element={
             <Layout>
-              {" "}
-              <UsePolicy />{" "}
+              <UsePolicy />
             </Layout>
           }
         />
@@ -151,44 +358,32 @@ function App() {
           path="/legal-disclaimer"
           element={
             <Layout>
-              {" "}
-              <LegalDisclaimer />{" "}
+              <LegalDisclaimer />
             </Layout>
           }
         />
-        {/* <Route path="/our-analyst" element={<Layout> <OurAnalyst/> </Layout>} /> */}
         <Route
           path="/our-products"
           element={
             <Layout>
-              {" "}
-              <Products />{" "}
+              <Products />
             </Layout>
           }
         />
-
-        {/* <Route path="/academy/:courseId" element={<ValourAcademy />} /> */}
-
         <Route path="/academy/:courseId" element={<CourseDetails />} />
         <Route
           path="/request-a-demo"
           element={
             <Layout>
-              {" "}
-              <RequestDemo />{" "}
+              <RequestDemo />
             </Layout>
           }
         />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/Privacy-Policy" element={<PrivacyPolicy />} />
-        <Route path="/contact" element={<ContactPage />}></Route>
-        {/* =========Platinum Dashboard======= */}
-        <Route
-          path="/platinum-dashboard"
-          element={<PlatinumDashboard />}
-        ></Route>
-        {/* <Route path="/view-challenges" element={<ViewChallenge />}></Route> */}
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/platinum-dashboard" element={<PlatinumDashboard />} />
         <Route path="/challenges/:id" element={<ViewChallenge />} />
       </Routes>
     </Router>
