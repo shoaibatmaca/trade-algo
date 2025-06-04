e3f44d9e60bc5c87c8231df00ce53d3eda10ad5f
Muhammad Shoaib <mshoaibkaglur@gmail.com>
2025-06-04 17:06:10 +0500

    dggeghegjgg

--- src/components/DashboardPlatinum/WeeklyBriefing.js
+++ src/components/DashboardPlatinum/WeeklyBriefing.js
@@ -426,6 +426,7 @@ import {
   faChevronRight,
   faLongArrowAltDown,
   faLongArrowAltUp,
+  faPlay,
   faStar,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
